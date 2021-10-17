e5f70c9b07e241da5136a35b303d847de55cbded
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-17 21:58:28 +0300

    Examble (1)

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+// Examble ArithmeticException (1)
+
+    var a = 5
+    var b = 0
+    println(a/b)
+
+/* Ans: Exception happened when we have division "a" on "b",
+   which "b" equal zero, meaning that the value of an operation will give infinite number,
+   this gives me an exception Type of  */
+
+}
